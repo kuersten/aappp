528ee68150575ccd4ad99a81d8fa1186c8df616c
Ruediger Kuersten <kursten@ub.edu>
2023-01-09 12:37:01 +0100

    changed version to 1.3 in setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ import subprocess
 
 extra_c=subprocess.getoutput('pkg-config --cflags glib-2.0').split()
 extra_l=subprocess.getoutput('pkg-config --libs glib-2.0').split()
-setup(name="aappp", license="MIT", description="Simulation package for Vicsek like models.", version="1.2", author="Ruediger Kuersten", url="https://github.com/kuersten/aappp/", ext_modules=[Extension('aappp', ['src/aapppmodule.c', 'src/c_functions_aappp.c'], extra_link_args=extra_l, extra_compile_args=extra_c)])
+setup(name="aappp", license="MIT", description="Simulation package for Vicsek like models.", version="1.3", author="Ruediger Kuersten", url="https://github.com/kuersten/aappp/", ext_modules=[Extension('aappp', ['src/aapppmodule.c', 'src/c_functions_aappp.c'], extra_link_args=extra_l, extra_compile_args=extra_c)])
