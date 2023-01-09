87f225717b5b0f25efb38482c89010ebff94bff2
Ruediger Kuersten <kursten@ub.edu>
2023-01-09 12:44:17 +0100

    updated install.bash

--- install.bash
+++ install.bash
@@ -2,6 +2,6 @@
 
 #CFLAGS="-I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include" python3 setup.py sdist
 python3 setup.py sdist	#compile package
-pip3 install ./dist/aappp-1.2.tar.gz	#install package
+pip3 install ./dist/aappp-1.3.tar.gz	#install package
 #without pip try (for local installation)
 #python3 setup.py install --user
